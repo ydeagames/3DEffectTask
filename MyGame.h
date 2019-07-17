7eeb1521d36551e992047071a3416f3a33813f97
YdeaGames <ydeagames@gmail.com>
2019-07-17 10:37:41 +0900

    Task_01_1

--- MyGame.h
+++ MyGame.h
@@ -11,6 +11,13 @@ class MyGame
 	// グリッド床
 	std::unique_ptr<GridFloor>				m_pGridFloor;
 
+	// エフェクト
+	std::unique_ptr<DirectX::BasicEffect> m_basicEffect;
+	// プリミティブバッチ
+	std::unique_ptr<DirectX::PrimitiveBatch<DirectX::VertexPositionColor>>	m_primitiveBatch;
+	// インプットレイアウト
+	Microsoft::WRL::ComPtr<ID3D11InputLayout> m_inputLayout;
+
 public:
 	// 生成
 	void Initialize(GameContext& context);
